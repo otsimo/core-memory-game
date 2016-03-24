5eb390cd212cfe408b0ee2ad9acb7d7961f9aa36
Sercan Degirmenci <degirmencisercan@gmail.com>
2016-03-24 13:48:52 +0200

    add npm publish

--- src/js/states/home.js
+++ src/js/states/home.js
@@ -5,7 +5,7 @@ export default class Home extends Phaser.State {
     create() {
         let music = this.game.add.audio('welcome');
         music.play();
-
+        
         this.game.add.button((this.game.width) * 0.37, (this.game.height) * 0.47, 'playButton', this.playAction, this, 2, 1, 0);
         this.game.add.button(25, 25, 'back', otsimo.quitgame, this);
     }
